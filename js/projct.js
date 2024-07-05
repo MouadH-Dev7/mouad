5d13603da174609d00186a13fcf252646d9269c7
MouadH-Dev7 <mouadh.dev@gmail.com>
2024-07-04 13:04:41 -1100

    Add files via upload

---
+++ js/projct.js
@@ -0,0 +1,22 @@
+var projcts = [
+    {
+        img: "images/Car finance-cuate.png",
+        titele: "Driving license management project",
+        description: "Driving license management project, programming in C#.  Net is a desktop application",
+
+    },
+    {
+        img: "images/Hospital building-bro.png",
+        titele: "Hospital management project",
+        description: "Hospital management project programmed in C#.  Net is a desktop application",
+
+    },
+    {
+        img: "images/Hotel Booking-bro.png",
+        titele: "Hotel management project",
+        description: "A hotel management project programmed in C#.  Net is a desktop application",
+
+    },
+
+
+]
